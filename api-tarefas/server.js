b335f13f63f1b5ee42b3fb3bb9664f777da95f07
Yan Amarante <yanlimaamarantec@hotmail.com>
2023-08-28 15:56:17 -0300

    versao final

--- api-tarefas/server.js
+++ api-tarefas/server.js
@@ -1,4 +1,14 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const port = 3000;
-//teste
+const tarefasRoutes = require("./src/tarefas/routes");
+
+
+app.use(bodyParser.json());
+
+app.use("/tarefas-api", tarefasRoutes);
+
+app.listen(port, () => {
+    console.log(`servidor rodando na porta: ${port}`);
+})
